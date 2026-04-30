8113297b457a78d397b336364cdc722a9af5b6f9
aaaylina <aylina.fedorova@mail.ru>
2026-04-30 16:03:58 +0300

    fix di recognizer

--- feature/test-creation/src/main/java/ru/itis/neuroteacher/testcreation/data/repository/TestRecognitionRepositoryImpl.kt
+++ feature/test-creation/src/main/java/ru/itis/neuroteacher/testcreation/data/repository/TestRecognitionRepositoryImpl.kt
@@ -2,17 +2,16 @@ package ru.itis.neuroteacher.testcreation.data.repository
 
 import android.graphics.Bitmap
 import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import com.google.mlkit.vision.text.TextRecognizer
 import kotlinx.coroutines.tasks.await
 import ru.itis.neuroteacher.testcreation.domain.repository.TextRecognitionRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TestRecognitionRepositoryImpl @Inject constructor() : TextRecognitionRepository {
-
-    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+class TestRecognitionRepositoryImpl @Inject constructor(
+    private val recognizer: TextRecognizer
+) : TextRecognitionRepository {
 
     override suspend fun recognizeText(bitmap: Bitmap): Result<String> {
         return runCatching {
